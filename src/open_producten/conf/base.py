e9607258381e91873e5c906ad789f0f1e200b74e
Floris272 <floris@maykinmedia.nl>
2024-12-03 17:45:56 +0100

    add page_size query param

--- src/open_producten/conf/base.py
+++ src/open_producten/conf/base.py
@@ -90,8 +90,8 @@ REST_FRAMEWORK = {
         "rest_framework.parsers.JSONParser",
     ],
     "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",),
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 100,
+    "DEFAULT_PAGINATION_CLASS": "open_producten.utils.pagination.Pagination",
+    # "PAGE_SIZE": 100,
 }
 
 #
