a32e813b45827109a358441533cb315dadd5c0dc
Floris272 <floris@maykinmedia.nl>
2024-12-04 16:01:50 +0100

    refactor routers & separate api specs

--- src/open_producten/producten/views.py
+++ src/open_producten/producten/views.py
@@ -1,10 +1,35 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from drf_spectacular.utils import extend_schema, extend_schema_view
 
 from open_producten.producten.models import Product
 from open_producten.producten.serializers.product import ProductSerializer
 from open_producten.utils.views import OrderedModelViewSet
 
 
+@extend_schema_view(
+    list=extend_schema(
+        summary="Alle PRODUCTEN opvragen.",
+        description="Deze lijst kan gefilterd wordt met query-string parameters.",
+    ),
+    retrieve=extend_schema(
+        summary="Een specifiek PRODUCT opvragen.",
+        description="Een specifieke PRODUCT opvragen.",
+    ),
+    create=extend_schema(
+        summary="Maak een PRODUCT aan.", description="Maak een PRODUCT aan."
+    ),
+    update=extend_schema(
+        summary="Werk een PRODUCT in zijn geheel bij.",
+        description="Werk een PRODUCT in zijn geheel bij.",
+    ),
+    partial_update=extend_schema(
+        summary="Werk een PRODUCT deels bij.", description="Werk een PRODUCT deels bij."
+    ),
+    destroy=extend_schema(
+        summary="Verwijder een PRODUCT.",
+        description="Verwijder een PRODUCT.",
+    ),
+)
 class ProductViewSet(OrderedModelViewSet):
     queryset = Product.objects.all()
     lookup_url_field = "id"
