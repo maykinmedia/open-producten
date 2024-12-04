74e9b5e8a8c654d39297f26ee5b18aca6cf03762
Floris272 <floris@maykinmedia.nl>
2024-12-04 11:08:46 +0100

    add published query param

--- src/open_producten/producten/views.py
+++ src/open_producten/producten/views.py
@@ -1,3 +1,5 @@
+from django_filters.rest_framework import DjangoFilterBackend
+
 from open_producten.producten.models import Product
 from open_producten.producten.serializers.product import (
     ProductSerializer,
@@ -9,6 +11,8 @@ from open_producten.utils.views import OrderedModelViewSet
 class ProductViewSet(OrderedModelViewSet):
     queryset = Product.objects.all()
     lookup_url_field = "id"
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["gepubliceerd"]
 
     def get_serializer_class(self):
         if self.action in ("update", "partial_update"):
