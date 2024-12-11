4106d2450ab59bd6ee3ff662a29c5a02c6e2b3e9
Floris272 <floris@maykinmedia.nl>
2024-12-11 12:38:39 +0100

    Add spectacular hook

---
+++ src/open_producten/utils/spectacular_hooks.py
@@ -0,0 +1,43 @@
+from sys import stdout
+
+
+def custom_postprocessing_hook(result, generator, request, public):
+    """
+    DRF Spectacular adds default = [] to the item inside an array on PrimaryKeyRelatedField fields with many=True like
+
+    `product_type_ids = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=ProductType.objects.all(),
+        default=[],
+        write_only=True,
+        source="product_typen",
+    )`
+    (src/open_producten/producttypen/serializers/onderwerp.py)
+
+    This generates the following schema:
+
+    product_type_ids:
+      type: array
+      items:
+        type: string
+        format: uuid
+        writeOnly: true     # duplicate
+        default: []         # duplicate and incorrect as the values in the array are type: string
+      writeOnly: true
+      default: []           # correct, type array has default: []
+    """
+
+    schemas = result.get("components", {}).get("schemas", {})
+
+    for schema_key, schema in schemas.items():
+        properties = schema.get("properties", {})
+
+        for prop_key, prop in properties.items():
+            default = prop.get("default", None)
+            if isinstance(default, list):
+                stdout.write(f"removed default=[] from {schema_key}.{prop_key}\n")
+                items = prop.get("items", {})
+                items.pop("default", None)
+                items.pop("writeOnly", None)
+
+    return result
