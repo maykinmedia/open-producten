bde792759f012576b37f7310a061c6dd95261ca7
Floris272 <floris@maykinmedia.nl>
2024-12-30 16:06:52 +0100

    rebase

--- src/open_producten/locaties/tests/api/test_contact.py
+++ src/open_producten/locaties/tests/api/test_contact.py
@@ -62,6 +62,7 @@ class TestContact(BaseApiTestCase):
             "organisatie": {
                 "id": str(contact.organisatie.id),
                 "naam": contact.organisatie.naam,
+                "code": contact.organisatie.code,
                 "email": contact.organisatie.email,
                 "telefoonnummer": contact.organisatie.telefoonnummer,
                 "straat": contact.organisatie.straat,
@@ -105,6 +106,7 @@ class TestContact(BaseApiTestCase):
                 "organisatie": {
                     "id": str(self.contact.organisatie.id),
                     "naam": self.contact.organisatie.naam,
+                    "code": self.contact.organisatie.code,
                     "email": self.contact.organisatie.email,
                     "telefoonnummer": self.contact.organisatie.telefoonnummer,
                     "straat": self.contact.organisatie.straat,
@@ -123,6 +125,7 @@ class TestContact(BaseApiTestCase):
                 "organisatie": {
                     "id": str(contact.organisatie.id),
                     "naam": contact.organisatie.naam,
+                    "code": contact.organisatie.code,
                     "email": contact.organisatie.email,
                     "telefoonnummer": contact.organisatie.telefoonnummer,
                     "straat": contact.organisatie.straat,
@@ -148,6 +151,7 @@ class TestContact(BaseApiTestCase):
             "organisatie": {
                 "id": str(self.contact.organisatie.id),
                 "naam": self.contact.organisatie.naam,
+                "code": self.contact.organisatie.code,
                 "email": self.contact.organisatie.email,
                 "telefoonnummer": self.contact.organisatie.telefoonnummer,
                 "straat": self.contact.organisatie.straat,
