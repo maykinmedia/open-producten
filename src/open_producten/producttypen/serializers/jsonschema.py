39d6144e4431cf15714da19cfd2705394bdba8df
Floris272 <floris@maykinmedia.nl>
2025-01-10 14:40:32 +0100

    add validate_schema to schemaSerializer

--- src/open_producten/producttypen/serializers/jsonschema.py
+++ src/open_producten/producttypen/serializers/jsonschema.py
@@ -1,10 +1,20 @@
 from django_json_schema.models import JsonSchema
+from jsonschema import Draft202012Validator
+from jsonschema.exceptions import SchemaError
 from rest_framework import serializers
 
 
 class JsonSchemaSerializer(serializers.ModelSerializer):
 
     schema = serializers.DictField()
+
+    def validate_schema(self, schema):
+        try:
+            Draft202012Validator.check_schema(schema)
+        except SchemaError as e:
+            raise serializers.ValidationError(e.message)
+
+        return schema
 
     class Meta:
         model = JsonSchema
