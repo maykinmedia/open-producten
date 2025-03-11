cc556a1d012ff974c21c021f0c8586343ec2f6c6
Floris272 <floris@maykinmedia.nl>
2025-03-11 13:48:19 +0100

    test release 0.0.5

--- README.rst
+++ README.rst
@@ -2,7 +2,7 @@
 open_producten
 ==================
 
-:Version: 0.0.4
+:Version: 0.0.5
 :Source: https://github.com/maykinmedia/open-producten
 :Keywords: ``producten``
 
--- src/open_producten/__init__.py
+++ src/open_producten/__init__.py
@@ -1,6 +1,6 @@
 from .celery import app as celery_app
 
 __all__ = ("celery_app",)
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 __author__ = "Maykin"
 __homepage__ = "https://github.com/maykinmedia/open-producten"
